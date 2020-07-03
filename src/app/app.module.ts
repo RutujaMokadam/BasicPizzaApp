b77b7f98416c27d214131214613dfb202b22dba0
RutujaMokadam <rutujav1994@gmail.com>
2020-07-03 13:38:44 +0530

    this is the pizza app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LogintoappComponent } from './logintoapp/logintoapp.component'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogintoappComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatButtonModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
